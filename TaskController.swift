e4d897330895b8b7521cc1f5a706aa30511da30d
Vishal Shelake <922.vishal@gmail.com>
2019-10-26 19:28:44 +0530

    a Simple ToDo Task App For iOS
    This is an Online Cource to Complete the MVC Architucture in iOS Development

---
+++ TaskController.swift
@@ -0,0 +1,13 @@
+//
+//  TaskController.swift
+//  Taskly
+//
+//  Created by Vishal Shelake on 26/10/19.
+//  Copyright © 2019 Vishal Shelake. All rights reserved.
+//
+
+import UIKit
+
+class TaskController : UITableViewController{
+     
+}
